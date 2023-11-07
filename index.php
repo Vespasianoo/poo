2435034095c4d5d7c56feb56b5f2be5b9ffad531
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-11-07 16:01:34 -0300

    corrigindo identação

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 spl_autoload_register(function($class) 
 {
-  if(file_exists('control/' . $class . '.php'))
-  {
-    require_once 'control/' .  $class . '.php';
-  }
+    if(file_exists('control/' . $class . '.php'))
+    {
+        require_once 'control/' .  $class . '.php';
+    }
 });
 
 $class = isset($_REQUEST['class']) ? $_REQUEST['class'] : 'PessoaList'; 
@@ -13,14 +13,15 @@ $method = isset($_REQUEST['method']) ? $_REQUEST['method'] : null;
   
 if(class_exists($class))
 {
-  $pagina = new $class($_REQUEST);
-  if(!empty($method) and method_exists($class, $method))
-  {
-    $pagina->$method($_REQUEST);
-  }
-  $pagina->show();
-} else {
-  $error404 = "<h1>404</h1> <br> <a href='index.php?class=PessoaList'>Ir para o inicio</a>";
-  print $error404;
+    $pagina = new $class($_REQUEST);
+    if(!empty($method) and method_exists($class, $method))
+    {
+        $pagina->$method($_REQUEST);
+    }
+    $pagina->show();
+} else 
+{
+    $error404 = "<h1>404</h1> <br> <a href='index.php?class=PessoaList'>Ir para o inicio</a>";
+    print $error404;
 }
 
