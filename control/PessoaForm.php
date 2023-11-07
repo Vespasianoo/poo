fa06a95fe5af01146cc56ca390d3ce941c135a18
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-11-07 16:52:25 -0300

    corrigindo identação

--- control/PessoaForm.php
+++ control/PessoaForm.php
@@ -3,59 +3,59 @@
 require_once "model/Pessoa.php";
 class PessoaForm 
 {
-  private $html;
-  private $data;
+    private $html;
+    private $data;
 
-  public function __construct()
-  {
-    $this->html = file_get_contents("html/form.html");
-    $this->data = ['id'       => '',
-                   'name'     => '',
-                   'email'    => '',
-                   'endereco' => '',
-                   'bairro'   => '',
-                   'tel'      => ''];
-  }
-
-  public function edit($params)  
-  {
-    try 
+    public function __construct()
     {
-      $id  = (int) $params['id'];
-      $this->data = Pessoa::find($id);
-    } 
-    catch (Exception $e) 
-    {
-      return $e->getMessage();
-    }  
-  }
+        $this->html = file_get_contents("html/form.html");
+        $this->data = [ 'id'       => '',
+                        'name'     => '',
+                        'email'    => '',
+                        'endereco' => '',
+                        'bairro'   => '',
+                        'tel'      => ''];
+    }
 
-  public function save($params)
-  {
-    try 
+    public function edit($params)  
     {
-      $pessoa = Pessoa::save($params);
-      $this->data = $pessoa;
-      $result = "Pessoa salva com sucesso <br> <a href='index.php'>Volta para pagina inicial</a>";
-      return  print $result; 
-    } 
-    catch (Exception $e) 
+        try 
+        {
+            $id  = (int) $params['id'];
+            $this->data = Pessoa::find($id);
+        } 
+        catch (Exception $e) 
+        {
+            return $e->getMessage();
+        }  
+    }
+
+    public function save($params)
     {
-      return $e->getMessage();
-    }  
-  }
+        try 
+        {
+            $pessoa = Pessoa::save($params);
+            $this->data = $pessoa;
+            $result = "Pessoa salva com sucesso <br> <a href='index.php'>Volta para pagina inicial</a>";
+            return  print $result; 
+        } 
+        catch (Exception $e) 
+        {
+            return $e->getMessage();
+        }  
+    }
 
-  public function show()  
-  {
-    $this->html = str_replace('{id}',       $this->data['id'],       $this->html);
-    $this->html = str_replace('{name}',     $this->data['name'],     $this->html);
-    $this->html = str_replace('{email}',    $this->data['email'],    $this->html);
-    $this->html = str_replace('{endereco}', $this->data['endereco'], $this->html);
-    $this->html = str_replace('{bairro}',   $this->data['bairro'],   $this->html);
-    $this->html = str_replace('{tel}',      $this->data['tel'],      $this->html);
+    public function show()  
+    {
+        $this->html = str_replace('{id}',       $this->data['id'],       $this->html);
+        $this->html = str_replace('{name}',     $this->data['name'],     $this->html);
+        $this->html = str_replace('{email}',    $this->data['email'],    $this->html);
+        $this->html = str_replace('{endereco}', $this->data['endereco'], $this->html);
+        $this->html = str_replace('{bairro}',   $this->data['bairro'],   $this->html);
+        $this->html = str_replace('{tel}',      $this->data['tel'],      $this->html);
 
-    print $this->html;
-  }
+        print $this->html;
+    }
 
     public function __call($method, $values) 
     {
--- control/PessoaList.php
+++ control/PessoaList.php
@@ -7,60 +7,59 @@ class PessoaList
     private $data;
 
     public function __construct()
-  {
-    $this->data['html']  = file_get_contents('html/list.html');
-    $this->data['items'] = '';
-  }
-
-  public function delete($params)
-  {
-    try
     {
-      $id = (int) $params['id'];
-      Pessoa::delete($id);
-      return header("Location: index.php");
+        $this->data['html']  = file_get_contents('html/list.html');
+        $this->data['items'] = '';
     }
-    catch (Exception $e)
+
+    public function delete($params)
     {
-      return print $e->getMessage();
+        try
+        {
+            $id = (int) $params['id'];
+            Pessoa::delete($id);
+            return header("Location: index.php");
+        }
+        catch (Exception $e)
+        {
+            return print $e->getMessage();
+        }
     }
-  }
 
-  public function load()
-  {
-    try
+    public function load()
     {
-      $pessoas = Pessoa::all();
-      // $items = ''; // ATR
+        try
+        {
+            $pessoas = Pessoa::all();
 
-      foreach ($pessoas as $pessoa)
-      {
-        $item = file_get_contents('html/item.html');
+            foreach ($pessoas as $pessoa)
+            {
+                $item = file_get_contents('html/item.html');
 
-        $item = str_replace('{id}',       $pessoa['id'],       $item);
-        $item = str_replace('{name}',     $pessoa['name'],     $item);
-        $item = str_replace('{email}',    $pessoa['email'],    $item);
-        $item = str_replace('{endereco}', $pessoa['endereco'], $item);
-        $item = str_replace('{bairro}',   $pessoa['bairro'],   $item);
-        $item = str_replace('{tel}',      $pessoa['tel'],      $item);
+                $item = str_replace('{id}',       $pessoa['id'],       $item);
+                $item = str_replace('{name}',     $pessoa['name'],     $item);
+                $item = str_replace('{email}',    $pessoa['email'],    $item);
+                $item = str_replace('{endereco}', $pessoa['endereco'], $item);
+                $item = str_replace('{bairro}',   $pessoa['bairro'],   $item);
+                $item = str_replace('{tel}',      $pessoa['tel'],      $item);
 
-        $this->data['items'] .= $item;
-      }
+                $this->data['items'] .= $item;
+            }
 
-     return $this->data['html'] = str_replace('{items}', $this->data['items'], $this->data['html']);
+            return $this->data['html'] = str_replace('{items}', $this->data['items'], $this->data['html']);
+        }
+        catch (Exception $e)
+        {
+            return print $e->getMessage();
+        }
     }
-    catch (Exception $e)
+
+    public function show()
     {
-      return print $e->getMessage();
+        $this->load();
+        print $this->data['html'];
+        return;
     }
-  }
-
-  public function show()
-  {
-    $this->load();
-    print $this->data['html'];
-    return;
-  }
 
     public function __call($method, $values) 
     {
